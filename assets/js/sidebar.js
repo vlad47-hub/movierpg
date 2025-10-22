12eb02770fd3baa247ff9515a258acd9d5669176
vlad47-hub <sanjeevgirwalkar@gmail.com>
2025-10-22 16:02:12 +0530

    Update sidebar.js

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -36,8 +36,7 @@ export function sidebar() {
       </div>
       <div class="sidebar-footer">
         <p class="copyright">Copyright 2023</p>
-        <a href="https://github.com/shivam171" style="font-size:15px"><img src="https://img.icons8.com/ios-filled/25/ffffff/github.png" alt="github"/>shivam171</a>
-        <br/>
+        
         <img
           src="./assets/images/tmdb-logo.png"
           alt="the movie database logo"
